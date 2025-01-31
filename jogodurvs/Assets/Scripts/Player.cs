fef5a6e98c1b81bba33583a43a1c6707dd9d7940
yasmim <yasmim.souza@escolar.ifrn.edu.br>
2025-01-31 11:08:58 -0300

    bug do pulo

--- jogodurvs/Assets/Scripts/Player.cs
+++ jogodurvs/Assets/Scripts/Player.cs
@@ -50,4 +50,20 @@ public class Player : MonoBehaviour
         }
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 6)
+        {
+            isJumping = true;
+        }
+    }
 }
