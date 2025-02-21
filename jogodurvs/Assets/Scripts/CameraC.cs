e229225010ce511708cb360315edc95fd02ea56e
IsacGu3des <isac.pinheiro@escolar.ifrn.edu.br>
2025-02-21 09:43:56 -0300

    camera

---
+++ jogodurvs/Assets/Scripts/CameraC.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraC : MonoBehaviour
+{
+    
+    private Transform alvo;
+    
+
+    private Vector3 direcao;
+
+    private float y;
+
+    private float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alvo = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Local();    
+    }
+
+    void Local()
+    {
+        y = 0;
+        x = alvo.position.x;
+        
+        if (x < 0)
+           x = 0;
+        direcao = new Vector3(x, y, -15f);
+        transform.position = direcao;
+    }
+}
